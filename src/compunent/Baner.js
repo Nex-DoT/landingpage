ecddbc7e429883ae8fef3f35e05ee42100798d49
NEX-DoT <nex.dot2@gmail.com>
2023-04-08 15:04:16 +0430

    navbar + Baner

---
+++ src/compunent/Baner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from "./Baner.module.css";
+
+
+const Baner = () => {
+    return (
+        <>
+        <div className={styles.baner}>
+             <div className={styles.textBox}>
+                 <h1>BOTOSTART</h1>
+                 <h3>we're learning <span>  React.js </span></h3>
+             </div>
+        </div>   
+        </>
+    );
+};
+
+export default Baner;
---
+++ src/compunent/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Navbar.module.css" 
+import JSlogo from "../img/JavaScript-logo.png"
+import REACTlogo from "../img/react-1.svg"
+const Navbar = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.listCom}>
+                <ul className={styles.list}>
+                    <li>Home Page</li>
+                    <li>Producs</li>
+                    <li>Aboute Us</li>
+                </ul>
+            </div>
+            <div className={styles.Img}>
+                <img src={JSlogo} alt="" />
+                <img src={REACTlogo} alt="" />
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
