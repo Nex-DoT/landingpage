9fcd65bf584b610889b2a2dcd453f771dde23e33
NEX-DoT <nex.dot2@gmail.com>
2023-04-12 14:33:19 +0430

    + & - added

--- src/compunent/Card.js
+++ src/compunent/Card.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import styles from "./Card.module.css"
 
 export default class Card extends Component {
+  constructor(){
+    super()
+    this.state = {
+      number: 0 ,
+    }
+  }
+  Minus = ()=>{
+    this.setState(prevState=>({
+      number: prevState.number -1,
+    }))
+
+    if(this.state.number === 0){
+      this.setState({
+        number: 0,
+      })
+    }
+  }
+  Positive = ()=>{
+    this.setState(prevState=> ({
+      number : prevState.number +1,
+    }))
+  }
   render() {
     const {name,img,price} =this.props;
     return (
@@ -11,7 +33,11 @@ export default class Card extends Component {
         <h3>{name}</h3>
         <p>price: <span>${price}</span></p>
         </div>
-        <button className={styles.btn}>+</button>
+        <div className={styles.boxc}>
+        <button className={this.state.number ? styles.btn : styles.deactive} onClick={this.Minus}>-</button>
+        <span>{this.state.number}</span>
+        <button className={styles.btn} onClick={this.Positive}>+</button>
+        </div>
       </div>
     )
   }
--- src/compunent/Sercher.js
+++ src/compunent/Sercher.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import styles from "./Sercher.module.css"
 export class Sercher extends Component {
+  constructor(){
+    super()
+    this.state={
+      text : "",
+    }
+  }
+  Chenge = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
   render() {
     return (
       <div className={styles.back}>
         <div className={styles.box}>
         <h1>Search what you need!</h1>
-        <input type="text" placeholder='search' />
+        <input type="text" placeholder='search' value={this.state.text} onChange={this.Chenge} />
         </div>
       </div>
     )
